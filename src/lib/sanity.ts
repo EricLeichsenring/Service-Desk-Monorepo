35489c9bac53eef16d52e99914d82ed8eb7ec5b7
Eric Leichsenring <eric.leichsenring@gmail.com>
2025-12-30 21:51:04 -0300

    Fix: Hardcoding token to resolve permission error

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,11 +2,11 @@ import { createClient } from "@sanity/client";
 import { createImageUrlBuilder } from '@sanity/image-url';
 
 export const client = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID, 
-  dataset: import.meta.env.VITE_SANITY_DATASET,
-  useCdn: false, // Deixe false para atualizações em tempo real
+  projectId: 's0a4d0sr', // Coloquei direto aqui
+  dataset: 'production', // Coloquei direto aqui
+  useCdn: false,
   apiVersion: '2023-05-03', 
-  token: import.meta.env.VITE_SANITY_TOKEN, // Seu token de escrita (Editor)
+  token: 'skOu8f2iUopTuMcORJBtqWOgiPyviUsn7XFxZ37yE3wumNuXNsxwXuzlNPtQnfm0TbInvw2fHnI0rCFr8n2LDuEpcigWJhHce1iHfmAHOH10XPGVJySrrNcA6zrwBt5bZ53VsewMq7uQUecQ1A0QYLJdeq1ryJFoSKAjnFCjHUW4ZVY2MvGc', 
 });
 
 // Configuração do construtor de imagens
