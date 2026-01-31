55236dfd3c6ecf84f946863a9628ddad9f834b7d
Eric Leichsenring <eric.leichsenring@gmail.com>
2026-01-31 10:07:31 -0300

    fix: explicit cors configuration for production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // --- ADICIONE ESTAS LINHAS ---
-  app.enableCors(); // Libera o Frontend para acessar o Backend
+  app.enableCors({
+    origin: '*', // Permite qualquer origem (ideal para teste inicial)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe()); // Ativa as validações (IsString, IsNotEmpty)
   // ----------------------------
 
